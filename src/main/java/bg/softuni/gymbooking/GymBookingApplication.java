e0c505324a7dae01bfc76d9ab07d9b3d215a2a4a
Simeon Rosenov Simeonov <simeonsimeonov006@gmail.com>
2026-06-13 19:56:11 +0300

    chore: scaffold Spring Boot project with Maven wrapper and dependencies

---
+++ src/main/java/bg/softuni/gymbooking/GymBookingApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.gymbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBookingApplication.class, args);
+    }
+
+}
